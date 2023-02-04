df6b2e4ac1076adb38377f68e51a858ac4f2ff25
Isaac177 <isaacjogues177@gmail.com>
2023-02-04 23:00:37 +0600

    Migration

--- ucm/settings.py
+++ ucm/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,7 +54,7 @@ ROOT_URLCONF = "ucm.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "ucm.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'unihub',
+        'USER': 'postgres',
+        'PASSWORD': 'francerdc1',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
