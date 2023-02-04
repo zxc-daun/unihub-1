8e42fa8d4039757f81b28ea6ab883340ef97b27f
Isaac177 <isaacjogues177@gmail.com>
2023-02-04 18:47:32 +0600

    hide key

--- unihub/unihub/settings.py
+++ unihub/unihub/settings.py
@@ -74,11 +74,16 @@ WSGI_APPLICATION = "unihub.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'unihub',
+        'USER': 'postgres',
+        'PASSWORD': 'francerdc1',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
