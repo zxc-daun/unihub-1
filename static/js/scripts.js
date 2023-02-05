0d27987db66377fff2a4d175cd32189add127068
Isaac177 <isaacjogues177@gmail.com>
2023-02-05 18:20:21 +0600

    Styles

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -4,3 +4,7 @@ const menu = document.querySelector(".navbar__menu");
 toggle.addEventListener("click", function() {
   menu.classList.toggle("navbar__menu--active");
 });
+
+const currentYear = new Date().getFullYear();
+const copyright = document.querySelector('footer p');
+copyright.textContent = `Copyright © ${currentYear}`;
