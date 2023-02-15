a4dbae209d9610a5c580d60b1510d9a6226b2137
Isaac177 <isaacjogues177@gmail.com>
2023-02-15 23:40:58 +0600

    Landing 2 sections

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,11 +1,46 @@
 const toggle = document.querySelector(".navbar__toggle");
 const menu = document.querySelector(".navbar__menu");
 const search = document.querySelector(".navbar__search");
+const carousel = document.querySelector('#our-clubs__carousel');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
 
 toggle.addEventListener("click", function() {
   menu.classList.toggle("navbar__menu--active");
 });
+// Select the carousel and controls
 
+
+// Set the initial position of the carousel
+let scrollPos = 0;
+
+// Set up the event listeners for the controls
+prevBtn.addEventListener('click', () => {
+carousel.scrollBy({
+left: -300,
+behavior: 'smooth'
+});
+});
+
+nextBtn.addEventListener('click', () => {
+carousel.scrollBy({
+left: 300,
+behavior: 'smooth'
+});
+});
+
+// Update the scroll position variable when the user scrolls
+carousel.addEventListener('scroll', () => {
+scrollPos = carousel.scrollLeft;
+});
+
+// Reset the scroll position if the user resizes the window
+window.addEventListener('resize', () => {
+carousel.scrollTo({
+left: scrollPos,
+behavior: 'auto'
+});
+});
 const currentYear = new Date().getFullYear();
 const copyright = document.querySelector('footer p');
 copyright.textContent = `Copyright © ${currentYear}`;
