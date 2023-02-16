ea0c53ab3c31023f88c582d11b0a5533f731ffdc
Isaac177 <isaacjogues177@gmail.com>
2023-02-16 10:10:48 +0600

    Carousel

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,18 +1,13 @@
 const toggle = document.querySelector(".navbar__toggle");
 const menu = document.querySelector(".navbar__menu");
 const search = document.querySelector(".navbar__search");
-const carousel = document.querySelector('#our-clubs__carousel');
-const prevBtn = document.querySelector('.prev');
-const nextBtn = document.querySelector('.next');
+
 
 toggle.addEventListener("click", function() {
   menu.classList.toggle("navbar__menu--active");
 });
 // Select the carousel and controls
 
-
-// Set the initial position of the carousel
-let scrollPos = 0;
 
 // Set up the event listeners for the controls
 prevBtn.addEventListener('click', () => {
@@ -44,3 +39,31 @@ behavior: 'auto'
 const currentYear = new Date().getFullYear();
 const copyright = document.querySelector('footer p');
 copyright.textContent = `Copyright © ${currentYear}`;
+
+const carousel = document.querySelector('#our-clubs__carousel');
+const prevBtn = document.querySelector('.our-clubs__prev-btn');
+const nextBtn = document.querySelector('.our-clubs__next-btn');
+
+// Set the initial position of the carousel
+let scrollPos = 0;
+
+// Set up the event listeners for the controls
+prevBtn.addEventListener('click', () => {
+  carousel.scrollBy({
+    left: -600,
+    behavior: 'smooth'
+  });
+});
+
+nextBtn.addEventListener('click', () => {
+  carousel.scrollBy({
+    left: 600,
+    behavior: 'smooth'
+  });
+});
+
+// Update the scroll position variable when the user scrolls
+carousel.addEventListener('scroll', () => {
+  scrollPos = carousel.scrollLeft;
+});
+
