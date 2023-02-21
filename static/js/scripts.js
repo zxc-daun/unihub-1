8b755e189c9ecd422d1e5a1d992032e60f7d0232
Isaac177 <isaacjogues177@gmail.com>
2023-02-21 09:07:29 +0600

    Admin route

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -6,30 +6,7 @@ const search = document.querySelector(".navbar__search");
 toggle.addEventListener("click", function() {
   menu.classList.toggle("navbar__menu--active");
 });
-// Select the carousel and controls
 
-
-// Set up the event listeners for the controls
-prevBtn.addEventListener('click', () => {
-carousel.scrollBy({
-left: -300,
-behavior: 'smooth'
-});
-});
-
-nextBtn.addEventListener('click', () => {
-carousel.scrollBy({
-left: 300,
-behavior: 'smooth'
-});
-});
-
-// Update the scroll position variable when the user scrolls
-carousel.addEventListener('scroll', () => {
-scrollPos = carousel.scrollLeft;
-});
-
-// Reset the scroll position if the user resizes the window
 window.addEventListener('resize', () => {
 carousel.scrollTo({
 left: scrollPos,
@@ -50,14 +27,14 @@ let scrollPos = 0;
 // Set up the event listeners for the controls
 prevBtn.addEventListener('click', () => {
   carousel.scrollBy({
-    left: -600,
+    left: -300,
     behavior: 'smooth'
   });
 });
 
 nextBtn.addEventListener('click', () => {
   carousel.scrollBy({
-    left: 600,
+    left: 300,
     behavior: 'smooth'
   });
 });
